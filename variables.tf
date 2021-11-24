de454537e2df0a12b640a3fe6dd529b47bc65b14
jpapazian2000 <jpapazian@hashicorp.com>
2021-11-24 11:04:06 +0100

    add ngins servers numners variable

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "prefix" {
  type = string
 }
 
+variable "nginx_servers" {
+   description = "number of server to start, 1 minimal and 4 maximum"
+   type = number
+
+   validation {
+     condition  = (
+      var.nginx_servers >= 1 &&
+      var.nginx_servers <= 4
+      )
+     error_message = "The number of nginx servers must be an integer between 1 and 4."
+
+
